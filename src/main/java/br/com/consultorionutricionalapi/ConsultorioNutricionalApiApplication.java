a6d34c6177d18ea4e3783133cc1cdf899d193ae2
Mauricio Carvalho <mauricio.cac@hotmail.com>
2021-05-11 16:55:13 -0300

    #1105 seccond commit

---
+++ src/main/java/br/com/consultorionutricionalapi/ConsultorioNutricionalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.consultorionutricionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioNutricionalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioNutricionalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/consultorionutricionalapi/ConsultorioNutricionalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultorionutricionalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioNutricionalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

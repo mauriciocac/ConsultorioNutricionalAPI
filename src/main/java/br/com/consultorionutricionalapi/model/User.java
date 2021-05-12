a20075e8a1b4665d0717632d9ebd69aa4d3957ea
Mauricio Carvalho <mauricio.cac@hotmail.com>
2021-05-12 13:34:39 -0300

    #1205 implementando a abstractEntity

--- src/main/java/br/com/consultorionutricionalapi/model/User.java
+++ src/main/java/br/com/consultorionutricionalapi/model/User.java
@@ -1,21 +1,14 @@
 package br.com.consultorionutricionalapi.model;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
-public class User implements Serializable {
+public class User extends AbstractEntity<Long> {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	@Column(name = "name")
 	private String name;
 	@Column(name = "cpf")
@@ -26,20 +19,11 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(Long id, String name, String cpf, LocalDate dataNascimento) {
+	public User(String name, String cpf, LocalDate dataNascimento) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getName() {
@@ -64,31 +48,6 @@ public class User implements Serializable {
 
 	public void setDataNascimento(LocalDate dataNascimento) {
 		this.dataNascimento = dataNascimento;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
